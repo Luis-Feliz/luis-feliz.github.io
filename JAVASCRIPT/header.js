1079283e9c31da380ec007dde3d8c0677e8bdde8
Luis Feliz <84045948+Luis-Feliz@users.noreply.github.com>
2023-05-23 22:00:22 -0400

    Update header.js

--- JAVASCRIPT/header.js
+++ JAVASCRIPT/header.js
@@ -15,22 +15,22 @@ class MyHeader extends HTMLElement
                         <div class="dropdown">
                             <button class="dropbtn"><strong>Portfolio</strong></button>
                             <div class="dropdown-content">
-                                <a href="GamingProject.html">Gaming Projects</a>
-                                <a href="CodingProject.html">Coding Projects</a>
-                                <a href="ArtProject.html">Art Projects</a>
-                                <a href="MusicProject.html">Music Projects</a>
-                                <a href="Future.html">What I am Working on now</a>
+                                <a href="HTML/GamingProject.html">Gaming Projects</a>
+                                <a href="HTML/CodingProject.html">Coding Projects</a>
+                                <a href="HTML/ArtProject.html">Art Projects</a>
+                                <a href="HTML/MusicProject.html">Music Projects</a>
+                                <a href="HTML/Future.html">What I am Working on now</a>
                             </div>
                         </div>
                     </th>  <!-- 2  -->
                     <th>
                         <div class="button">
-                            <a href="AboutMe.html" class="button"> About Me</a>
+                            <a href="HTML/AboutMe.html" class="button"> About Me</a>
                         </div>
                     </th>  <!-- 3 -->
                     <th>
                         <div class="button">
-                            <a href="ContactMe.html" class="button">Contact Me</a>
+                            <a href="HTML/ContactMe.html" class="button">Contact Me</a>
                         </div>
                     </th>  <!-- 4 -->
                 </tr>
