aeb2455d0531de6da98bfffda07a93715631dba2
Luis Feliz <luisfeliz42100@gmail.com>
2023-07-06 15:33:20 -0400

    3

--- JAVASCRIPT/header.js
+++ JAVASCRIPT/header.js
@@ -6,36 +6,46 @@ class MyHeader extends HTMLElement
         `   
             <table>
                     <th>
-                        <div class="button">
-                            <a href="index.html" class="button">Home Page</a>
-                        </div>
+                        <td>
+                            <div class="button">
+                                <a href="index.html" class="button">Home Page</a>
+                            </div>
+                        </td>
                     </th>  <!-- 1 -->
                     <th>
-                        <div class="button">
-                            <a href="index.html" class="button">Skills</a>
-                        </div>
+                        <td>
+                            <div class="button">
+                                <a href="index.html" class="button">Skills</a>
+                            </div>
+                        </td>
                     </th>  <!-- 2 -->
                     <th>
-                        <div class="dropdown">
-                            <button class="dropbtn"><strong>Portfolio</strong></button>
-                            <div class="dropdown-content">
-                                <a href="GamingProject.html">Gaming Projects</a>
-                                <a href="CodingProject.html">Coding Projects</a>
-                                <a href="ArtProject.html">Art Projects</a>
-                                <a href="MusicProject.html">Music Projects</a>
-                                <a href="Future.html">Future Projects</a>
+                        <td>
+                            <div class="dropdown">
+                                <button class="dropbtn"><strong>Portfolio</strong></button>
+                                <div class="dropdown-content">
+                                    <a href="GamingProject.html">Gaming Projects</a>
+                                    <a href="CodingProject.html">Coding Projects</a>
+                                    <a href="ArtProject.html">Art Projects</a>
+                                    <a href="MusicProject.html">Music Projects</a>
+                                    <a href="Future.html">Future Projects</a>
+                                </div>
                             </div>
-                        </div>
+                        <td>
                     </th>  <!-- 3 -->
                     <th>
-                        <div class="button">
-                            <a href="AboutMe.html" class="button">About Me</a>
-                        </div>
+                        <td>
+                            <div class="button">
+                                <a href="AboutMe.html" class="button">About Me</a>
+                            </div>
+                        </td>
                     </th>  <!-- 4 -->
                     <th>
-                        <div class="button">
-                            <a href="ContactMe.html" class="button">Contact Me</a>
-                        </div>
+                        <td>
+                            <div class="button">
+                                <a href="ContactMe.html" class="button">Contact Me</a>
+                            </div>
+                        </td>
                     </th>  <!-- 5 -->
             </table>
         `
