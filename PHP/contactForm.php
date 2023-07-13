fa0323ea414cb6a97423af813cf0732f871cc045
Luis Feliz <luisfeliz42100@gmail.com>
2023-07-12 21:04:50 -0400

    7/12/2023 - 1
    After days of relaxation I've returned with progress

---
+++ PHP/contactForm.php
@@ -0,0 +1,14 @@
+<?php
+if (isset($_POST['submit'])){
+    $name = $_POST['name'];
+    $mailFrom = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
+
+    $mailTo = "luisfeliz42100@gmail.com";
+    $headers = "From: ".$mailFrom;
+    $txt = "You have received an e-mail from".$name.".\n\n".$message;
+
+    mail($mailTo, $subject, $txt, $headers);
+    header("Location: contactForm.php?mailsend");
+}
