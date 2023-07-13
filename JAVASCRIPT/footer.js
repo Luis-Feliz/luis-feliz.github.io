efd8f38f8e0d2815cf9d30731e6af32ed0c68102
Luis Feliz <luisfeliz42100@gmail.com>
2023-07-13 16:16:18 -0400

    7/13
     Happy National French Fry Day!

--- JAVASCRIPT/footer.js
+++ JAVASCRIPT/footer.js
@@ -4,8 +4,12 @@ class MyFooter extends HTMLElement
     {
         this.innerHTML = 
         `
-            <h5>Developed by Luis Feliz</h5>
-            <p>*Links to my other sites*</p>
+            <h5><u>Developed by Luis Feliz</u></h5>
+            <a href="mailto: luisfeliz42100@gmail.com" target="_blank"><img src="IMAGES/gmail.png" alt="Gmail Pic" width="25" height="25"></a>
+            <a href="https://github.com/Luis-Feliz" target="_blank"><img src="IMAGES/github_black.png" alt="Github Pic" width="25" height="25"></a>            
+            <a href="https://www.linkedin.com/in/luis-feliz" target="_blank"><img src="IMAGES/linkdin.png" alt="linkdin Pic" width="25" height="25"></a>
+            <a href="https://twitter.com/LuisFel78699502" target="_blank"><img src="IMAGES/twitter.png" alt="Twitter Pic" width="25" height="25"></a>
+            <a href="#" target="_blank"><img src="IMAGES/youtube.svg" alt=Youtube Pic" width="25" height="25"></a>
         `
     }
 }
