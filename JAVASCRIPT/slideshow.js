70b9c1ee4c492244ea2f77b95a040ad0ac6caa73
Luis Feliz <luisfeliz42100@gmail.com>
2023-07-15 16:45:31 -0400

    7/15/2023 - 1
    Finished protfollio build setup. Website is 90% complete.

--- JAVASCRIPT/slideshow.js
+++ JAVASCRIPT/slideshow.js
@@ -5,13 +5,31 @@ function plusDivs(n) {
   showDivs(slideIndex += n);
 }
 
+function currentDiv(n) {
+  showDivs(slideIndex = n);
+}
+
 function showDivs(n) {
   var i;
   var x = document.getElementsByClassName("mySlides");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length};
+  var dots = document.getElementsByClassName("demo");
+  if (n > x.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = x.length}
   for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
+    x[i].style.display = "none";  
   }
-  x[slideIndex-1].style.display = "block";
+  x[slideIndex-1].style.display = "block"; 
+  
+  if (n > dots.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = dots.length}
+  for (i = 0; i < dots.length; i++) {
+    dots[i].style.display = "none";  
+  }
+  dots[slideIndex-1].style.display = "block"; 
+
+  // for (i = 0; i < dots.length; i++) {
+  //   dots[i].className = dots[i].className.replace("active");
+  // }
+   
+  // dots[slideIndex-1].className += " active";
 }
