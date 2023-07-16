ee446a947a39fa18bfbfebef0b3bbc565f3a5b49
Luis Feliz <luisfeliz42100@gmail.com>
2023-07-15 21:56:59 -0400

    3

--- PHP/contactForm.php
+++ PHP/contactForm.php
@@ -1,15 +1,32 @@
 <?php
-if (isset($_POST['submit'])){
-    $name = $_POST['name'];
-    $mailFrom = $_POST['email'];
-    $subject = $_POST['subject'];
-    $message = $_POST['message'];
+    if (isset($_POST['submit'])){
+        // validation expected data exists
+        if (!isset($_POST['fullName']) || !isset($_POST['email']) || !isset($_POST['message'])) {
+            echo "Oh looks like there is some problem with your form data.";
+        }
+        else{
+        
+            $name = $_POST['name'];
+            $mailFrom = $_POST['email'];
+            $subject = $_POST['subject'];
+            $message = $_POST['message'];
 
-    $mailTo = "luisfeliz42100@gmail.com";
-    $headers = "From: ".$mailFrom;
-    $txt = "You have received an e-mail from".$name.".\n\n".$message;
+            $mailTo = "luisfeliz42100@gmail.com";
+            $headers = "From: ".$mailFrom;
+            $txt = "You have received an e-mail from".$name.".\n\n".$message;
 
-    mail($mailTo, $subject, $txt, $headers);
-    header("Location: contactForm.php?mailsend");
-}
+
+            header($mailTo);
+            @mail($mailTo, $subject, $txt, $headers);
+            ?>
+            Message Sent! Thank you!    
+            <?php
+        }
+    }       
 ?>
+
+    
+
+
+        
+        
