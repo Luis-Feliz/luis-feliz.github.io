45cfffc6035ca1ba9bfc23de216ba4abbd523b4f
Luis Feliz <luisfeliz42100@gmail.com>
2023-07-20 01:01:51 -0400

    4

--- JAVASCRIPT/footer.js
+++ JAVASCRIPT/footer.js
@@ -6,7 +6,7 @@ class MyFooter extends HTMLElement
         `
             <h4><u>Developed by Luis Feliz</u></h4>
             <h5>Please reach out via any of the icons below or through my phone: 1(267)562-2810</h5>
-            <a href="mailto: luisfeliz42100@gmail.com" target="_blank"><img src="IMAGES/gmail.png" alt="Gmail Pic" width="25" height="25"></a>
+            <a href="mailto: luisfeliz42100@gmail.com" target="_blank"><img src="IMAGES/gmail.png" alt="Gmail Pic" width="25" height="25" color: white></a>
             <a href="https://github.com/Luis-Feliz" target="_blank"><img src="IMAGES/github_black.png" alt="Github Pic" width="25" height="25"></a>            
             <a href="https://www.linkedin.com/in/luis-feliz" target="_blank"><img src="IMAGES/linkdin.png" alt="linkdin Pic" width="25" height="25"></a>
             <a href="https://twitter.com/LuisFel78699502" target="_blank"><img src="IMAGES/twitter.png" alt="Twitter Pic" width="25" height="25"></a>
