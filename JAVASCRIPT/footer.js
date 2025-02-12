172bb43b0f55d3884d8a0cf652597200e3f676ad
Luis Feliz <luisfeliz42100@gmail.com>
2025-02-12 10:20:32 -0500

    OWT Update 2.0
    Added itch logo & updated OWT description with link

--- JAVASCRIPT/footer.js
+++ JAVASCRIPT/footer.js
@@ -10,6 +10,7 @@ class MyFooter extends HTMLElement
             <a href="https://www.linkedin.com/in/luis-feliz" target="_blank"><img src="IMAGES/linkdin_white.png" alt="linkdin Pic" width="25" height="25"></a>
             <a href="https://twitter.com/LuisFel78699502" target="_blank"><img src="IMAGES/twitter_white.png" alt="Twitter Pic" width="25" height="25"></a>
             <a href="#" target="_blank"><img src="IMAGES/youtube_white.png" alt=Youtube Pic" width="25" height="25"></a>
+            <a href="https://luis-feliz.itch.io/one-way-ticket" target="_blank"><img src="IMAGES/itchioLOGO.png" alt=Youtube Pic" width="25" height="25"></a>
         `
     }
 }
